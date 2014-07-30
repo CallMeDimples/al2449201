4f66a593130c1ed0f813d88fa223d3fbe60aaa2a
Allison Lomheim <allamaheim@hotmail.com>
2014-07-30 12:33:08 -0700

    Signed-off-by: Allison Lomheim <allamaheim@hotmail.com>

--- Projects/Project~2/nbproject/Makefile-variables.mk
+++ Projects/Project~2/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
-CND_ARTIFACT_NAME_Debug=project_1
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/project_1
+CND_ARTIFACT_NAME_Debug=project_2
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/project_2
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
-CND_PACKAGE_NAME_Debug=project~1.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/project~1.tar
+CND_PACKAGE_NAME_Debug=project~2.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/project~2.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
-CND_ARTIFACT_NAME_Release=project_1
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/project_1
+CND_ARTIFACT_NAME_Release=project_2
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/project_2
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
-CND_PACKAGE_NAME_Release=project~1.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/project~1.tar
+CND_PACKAGE_NAME_Release=project~2.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/project~2.tar
 #
 # include compiler specific variables
 #
